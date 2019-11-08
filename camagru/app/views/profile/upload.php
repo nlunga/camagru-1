365d48a14d501becfb165010b5942859883a518c
vesingh <vesingh@student.wethinkcode.co.za>
2019-11-08 13:31:02 +0200

    headlines added to all pages

--- camagru/app/views/profile/upload.php
+++ camagru/app/views/profile/upload.php
@@ -3,6 +3,6 @@
 <?php $this->start('body'); ?>
 <div class="page-header text-center">
   <h1>Sup <?=ucwords(currentUser()->fname)?><br>
-  	<small>Upload</small></h1>
+  	<small>Lets create shit</small></h1>
 </div>
 <?php $this->end(); ?>
